8bc8b9f2c8ee16d3807e21bc649ba2ccbbf90cc0
Maxim <hristivi8383@gmail.com>
2026-04-22 23:56:51 +0300

    initial commit

---
+++ server/src/main/java/com/fmi/springcourse/marketplace/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.fmi.springcourse.marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+	
+	void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+	
+}
