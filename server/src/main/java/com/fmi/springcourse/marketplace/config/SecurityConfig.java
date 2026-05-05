70005d18f8b2b05ae0c2f6131d054d3cd78af232
Damyan Georgiev <damiangeorgiev03@gmail.com>
2026-05-05 15:42:50 +0300

    feat: implement base security configuration and refactor user repository
    - Configured SecurityConfig with BCryptPasswordEncoder
    - Moved UserRepository to the user package for better modularity
    - Note: spring-boot-starter-security dependency is already added

--- server/src/main/java/com/fmi/springcourse/marketplace/config/SecurityConfig.java
+++ server/src/main/java/com/fmi/springcourse/marketplace/config/SecurityConfig.java
@@ -1,20 +1,48 @@
 package com.fmi.springcourse.marketplace.config;
 
+import com.fmi.springcourse.marketplace.filter.JwtRequestFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+//	private final UserDetailsService userDetailsService;
+//	private final JwtRequestFilter jwtRequestFilter;
+
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http
-			.csrf(csrf -> csrf.disable())
-			.authorizeHttpRequests(auth -> auth
-				.anyRequest().permitAll()
-			);
-		
+	SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		http.csrf(AbstractHttpConfigurer::disable)
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/auth/register").permitAll()
+						.requestMatchers("/auth/login").permitAll()
+						.requestMatchers("/admin").hasRole("ADMIN")
+						.requestMatchers("/user").hasAnyRole("USER", "ADMIN")
+						.anyRequest().authenticated())
+				.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+//		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 		return http.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) {
+		return config.getAuthenticationManager();
 	}
 }
