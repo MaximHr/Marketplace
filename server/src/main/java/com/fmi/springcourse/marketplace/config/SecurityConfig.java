759f5c6f0b46598d3acb8ec0eda78c9874c32900
Maxim <hristivi8383@gmail.com>
2026-06-08 14:47:32 +0300

    cors allow front store

--- server/src/main/java/com/fmi/springcourse/marketplace/config/SecurityConfig.java
+++ server/src/main/java/com/fmi/springcourse/marketplace/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.fmi.springcourse.marketplace.config;
 
 import com.fmi.springcourse.marketplace.filter.JwtRequestFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,12 +11,14 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @RequiredArgsConstructor
@@ -23,9 +26,13 @@ public class SecurityConfig {
 	private final JwtRequestFilter jwtRequestFilter;
 	private final AuthenticationEntryPoint jwtAuthenticationEntryPoint;
 	
+	@Value("${store.front.url}")
+	private String storeUrl;
+	
 	@Bean
 	SecurityFilterChain configure(HttpSecurity http) throws Exception {
 		http
+			.cors(cors -> cors.configurationSource(corsConfigurationSource()))
 			.csrf(AbstractHttpConfigurer::disable)
 			.sessionManagement(session ->
 				session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -53,5 +60,22 @@ public class SecurityConfig {
 	@Bean
 	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) {
 		return config.getAuthenticationManager();
+	}
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration config = new CorsConfiguration();
+		
+		config.setAllowCredentials(true);
+		
+		config.addAllowedOrigin(storeUrl);
+		
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		
+		return source;
 	}
 }
